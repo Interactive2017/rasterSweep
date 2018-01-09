31b29b1f84a341ced873305cd2e84c350f02d88d
jansule <j_sule02@wwu.de>
2018-01-09 20:59:55 +0100

    overlay images and sweeper
      - sweeper hovers over image
      - hide mouse

--- rasterSweep.directive.js
+++ rasterSweep.directive.js
@@ -12,6 +12,23 @@
         };
 
         function link(scope, element, attrs){
+            console.log('element', element);
+            var img = element[0].children[1];
+            img.onmouseover = function(event){
+                // console.log('hovering...', event);
+                // console.log('layerX', JSON.stringify(event.layerX));
+                // console.log('pageX', JSON.stringify(event.pageX));
+                // console.log('clientX', JSON.stringify(event.clientX));
+                // console.log('screenX', JSON.stringify(event.screenX));
+                img.onmousemove = function(moveevent){
+                    var mouseX = moveevent.pageX - img.offsetLeft;
+                    var mouseY = moveevent.pageY - img.offsetTop;
+                    element[0].children[2].style.left = mouseX + 'px';
+                    element[0].children[2].style.top = mouseY + 'px';
+                }
+
+                
+            }
             console.log('hello world');
         }
     }
