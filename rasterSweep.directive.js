e9f5ac9a6ba7f4f0dd81a1782dcaadeb984cddda
Simon Schulte <simonschulte1991@gmail.com>
2018-01-16 10:21:42 +0100

    Canvas test to create diff image// CORS problems

--- rasterSweep.directive.js
+++ rasterSweep.directive.js
@@ -10,22 +10,83 @@
             restrict: 'E',
             link: link
         };
-
+        
         function link(scope, element, attrs){
-            // console.log('width', element[0].css('width'));
-            var img = element[0].children[1];
-            var sweeper = element.find('#raster-glass');
-            sweeper.css('width', img.width/10);
-            sweeper.css('height', img.width/10);
-            sweeper.css('border-width', img.width/100);
-            console.log('element', element);
-            img.onmousemove = function(moveevent){
-                var mouseX = moveevent.pageX - img.offsetLeft;
-                var mouseY = moveevent.pageY - img.offsetTop;
-                sweeper.css('left', mouseX + 'px');
-                sweeper.css('top', mouseY + 'px');
-                element.find('#raster-sweeper-top-image').css('clip-path', 'polygon('+ mouseX + 'px ' + mouseY + 'px, ' + mouseX+img.width + 'px '+ mouseY + 'px, ' + mouseX+img.width + 'px ' + mouseY+img.height + 'px, '+ mouseX +'px ' + mouseY+img.height + 'px)');
+            var firstImgPath = 'test/original.png';
+            var secondImgPath = 'test/modified.png';            
+            var canvas = document.getElementById('rasterSweepCanvas');
+            // console.log('canvas:', canvas)
+            var context = canvas.getContext('2d');
+            // console.log('context:', context)
+            loadImage(firstImgPath, context);
+            // loadImage(secondImgPath, context);
+
+            function loadImage(imagePath){
+                var image = new Image();
+                image.src = imagePath;
+                image.onload = function(){
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    context.drawImage(image, 0, 0);
+                    readAllPixels();
+                }
             }
+            function readAllPixels(){
+                for (var x = 0; x < canvas.width; x++) {
+                    for (var y = 0; y < canvas.height; y++) {  
+                        readRGBValue(x,y);
+                    }
+                }
+            }
+            function readRGBValue(x,y){
+                var rgbValue = context.getImageData(x,y,1,1).data;
+                var red = rgbValue[0];
+                // var green = rgbValue[1];
+                // var blue = rgbValue[2];
+                console.log(rgbValue);
+                if(red > 0){
+                    console.log(red);
+                }
+            }
+        }
+        
+        function createDiffMap(firstImage, secondImage){
+
         }
     }
 })();
+
+// Read RGB Color Inputs: Image, x, y Returns: RGB color string
+// retrieveRGB(image,x,y):
+//  red = image[x][y].[0]
+//  blue = image[x][y].[1]
+//  green = image[x][y].[2]
+//  alpha ? needed?
+// return red + " " + blue + " " + green
+
+// Diffmap function Inputs: Two Images Returns: Difference matrix
+// for loop x rows of images
+//  for loop y rows of images
+//     firstImageVal = retrieveRGB(firstImage,x,y);
+//     secondImageVal = retrieveRGB(firstImage,x,y);
+//     if(firstImageVal === secondImageVal){
+//         comparisonMatrix[x][y] = 0
+//      }  else {
+//     comparisonMatrix[x][y] = 1
+//  }
+// }
+
+
+
+
+
+// calculateIntegralImage Inputs: Difference Matrix from Diffmap function
+//value = 0
+//for loop x rows of image
+//  for loop y rows of image
+//   value = comparisonMatrix[x][y] + value
+//   IntegralImage[x][y] = value
+//  }
+// }
+    
+
