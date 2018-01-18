eb3617f6f872b38a0886e7cf166f8e29adf5d6f2
jansule <j_sule02@wwu.de>
2018-01-18 20:44:35 +0100

    show overlayed image within sweeper-glass
      - improve performance (how?)

--- rasterSweep.directive.js
+++ rasterSweep.directive.js
@@ -4,8 +4,8 @@
         .module('rasterSweep')
         .directive('rasterSweep', rasterSweep);
     
-    rasterSweep.$inject = [];
-    function rasterSweep(){
+    rasterSweep.$inject = ['$timeout'];
+    function rasterSweep($timeout){
         return {
             restrict: 'E',
             link: link
@@ -13,31 +13,32 @@
 
         function link(scope, element, attrs){
             console.log('width', element.next());
-            var img = element[0].children[0];
-            // var sweepDiv = element[0].innerHTML += '<div id="raster-glass"></div>';
-
+            var img = element.find('#raster-sweeper-top-image');
+            var dig = element.find('#digitiser');
+            dig.css('width', img[0].width + 'px'),
+            dig.css('height', img[0].height + 'px');
             var sweeper = element.find('#raster-glass');
             console.log('sweeper', sweeper);
-            sweeper.css('width', img.width/5);
-            sweeper.css('height', img.width/5);
-            sweeper.css('border-width', img.width/100);
-            console.log('img width', img.width);
-            console.log('element', element);
-            img.onmousemove = function(moveevent){
-                console.log('mouseevent)');
-                var mouseX = moveevent.pageX - img.offsetLeft;
-                var mouseY = moveevent.pageY - img.offsetTop;
-                if(mouseX > img.width - (img.width/5)) mouseX = img.width - (img.width/5);
-                if(mouseX < img.offsetLeft) mouseX = img.offsetLeft;
-                if(mouseY > img.height - (img.width/5)) mouseY = img.height - (img.width/5);
-                if(mouseY < img.offsetTop) mouseY = img.offsetTop;
+            sweeper.css('width', Math.floor(img[0].width/5));
+            sweeper.css('height', Math.floor(img[0].width/5));
+            var sweeperBorderWidth = Math.floor(img[0].width/100);
+            sweeper.css('border-width', sweeperBorderWidth + 'px');
+            img.css('opacity', '0');
+            dig[0].onmousemove = function(moveevent){
+                var mouseX = moveevent.pageX - img[0].offsetLeft;
+                var mouseY = moveevent.pageY - img[0].offsetTop;
+                if(mouseX > img[0].width - (img[0].width/5)) mouseX = img[0].width - (img[0].width/5);
+                if(mouseX < img[0].offsetLeft) mouseX = img[0].offsetLeft;
+                if(mouseY > img[0].height - (img[0].width/5)) mouseY = img[0].height - (img[0].width/5);
+                if(mouseY < img[0].offsetTop) mouseY = img[0].offsetTop;
                 sweeper.css('left', mouseX + 'px');
                 sweeper.css('top', mouseY + 'px');
-                // sweeper.css('transform', 'translateY('+ mouseY + 'px) translateX('+ mouseX + 'px)');
-                
-                // sweeper.style.transform = 'translateY('+ mouseY+'px)';
-                // sweeper.style.transform += 'translateX('+mouseX+'px)';
-                // element.find('#raster-sweeper-top-image').css('clip-path', 'polygon('+ mouseX + 'px ' + mouseY + 'px, ' + mouseX+img.width + 'px '+ mouseY + 'px, ' + mouseX+img.width + 'px ' + mouseY+img.height + 'px, '+ mouseX +'px ' + mouseY+img.height + 'px)');
+                var fromTop = mouseY;
+                var fromRight = img[0].width - Math.floor(img[0].width/5) - mouseX - sweeperBorderWidth;
+                var fromBottom = img[0].height - Math.floor(img[0].width/5) - mouseY - sweeperBorderWidth;
+                var fromLeft = mouseX;
+                img.css('clip-path', 'inset(' + fromTop + 'px ' + fromRight + 'px ' + fromBottom + 'px ' +  fromLeft + 'px)');
+                img.css('opacity', '1');
             }
         }
     }
